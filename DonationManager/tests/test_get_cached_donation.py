c2290b6addd8cd23d7f0f9f26de800c53f6f4fb9
fangorshile <jp8shields@gmail.com>
2026-03-24 20:53:28 -0400

    changed some output for pretty json

--- DonationManager/tests/test_get_cached_donation.py
+++ DonationManager/tests/test_get_cached_donation.py
@@ -1,4 +1,5 @@
 from donationmanager import DonoTracker
+import json
 
 url = "https://www.extra-life.org/api/1.6/participants/minismeef/donations"
 donotracker = DonoTracker.DonationTracker(url)
@@ -6,16 +7,16 @@ donotracker = DonoTracker.DonationTracker(url)
 if __name__ == "__main__":
     print("get_cached_donation(0) output:")
     last_donation = donotracker.get_cached_donation(0)
-    print(last_donation)
+    print(json.dumps(last_donation, indent=4))
 
     print("get_cached_donation(1) output:")
     donation_1 = donotracker.get_cached_donation(1)
-    print(donation_1)
+    print(json.dumps(donation_1, indent=4))
 
     print("out of bounds donation output:")
     donation_out_of_bounds = donotracker.get_cached_donation(999)
-    print(donation_out_of_bounds)
+    print(json.dumps(donation_out_of_bounds, indent=4))
 
     print("non-int index donation output:")
     donation_non_int = donotracker.get_cached_donation("1")
-    print(donation_non_int)
+    print(json.dumps(donation_non_int, indent=4))
